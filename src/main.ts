d50e62c73f44076c7667d888ea131ce1b8122ce4
Vladimir Agaev <agaev.vladimir@gmail.com>
2024-09-03 04:45:50 +0200

    fix: start script

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3335);
+  app.enableCors();
+  app.use(helmet());
+
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get('PORT');
+
+  await app.listen(port ?? 3333);
 }
 bootstrap();
