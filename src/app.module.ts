b9a67243a0bcbefc2066087a5366badd91cc1a93
Vladimir Agaev <agaev.vladimir@gmail.com>
2024-09-03 04:35:35 +0200

    chore: add health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@app/config';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, HealthModule],
   controllers: [AppController],
 })
 export class AppModule {}
