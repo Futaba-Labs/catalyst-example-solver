64c331382bd517be980122a240bc883d063a89ad
Vladimir Agaev <agaev.vladimir@gmail.com>
2024-09-03 05:17:30 +0200

    chore: update type

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,15 +14,38 @@ export interface CatalystQuoteRequestData {
 }
 
 export interface CatalystOrderData {
-  orderId: string;
-  quoteRequestId: string;
-  fromChain: string;
-  toChain: string;
-  fromAsset: string;
-  toAsset: string;
-  expirationTime: string;
-  amount: string;
-  status: string;
-  createdAt: string;
-  updatedAt: string;
+  orderKeyHash: string | null;
+  orderType: 'LimitOrder' | 'DutchAuction';
+  orderData: {
+    type: string;
+    inputs: any[];
+    outputs: any[];
+    inputSlopes: any[];
+    localOracle: string;
+    outputSlopes: any[];
+    proofDeadline: number;
+    collateralToken: string;
+    challengeDeadline: number;
+    slopeStartingTime: number;
+    verificationContext: string;
+    verificationContract: string;
+    fillerCollateralAmount: number;
+    challengerCollateralAmount: number;
+  };
+  quoteContext: {
+    // TODO: should we add a solver identifier here?
+    toAsset: string;
+    toPrice: string;
+    discount: string;
+    fromAsset: string;
+    fromPrice: string;
+    intermediary: string;
+  };
+  settlementContractAddress: string;
+  swapperAddress: string;
+  nonce: number;
+  originChainId: number;
+  initiatedDeadline: number;
+  fillDeadline: number;
+  signature: string;
 }
