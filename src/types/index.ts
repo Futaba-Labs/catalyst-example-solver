1216ec2db65a09fc967c0b701bd0405bc3ce8a19
Vladimir Agaev <agaev.vladimir@gmail.com>
2024-09-04 15:13:47 +0200

    chore: update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,39 +13,69 @@ export interface CatalystQuoteRequestData {
   amount: string;
 }
 
-export interface CatalystOrderData {
-  orderKeyHash: string | null;
-  orderType: 'LimitOrder' | 'DutchAuction';
-  orderData: {
-    type: string;
-    inputs: any[];
-    outputs: any[];
-    inputSlopes: any[];
-    localOracle: string;
-    outputSlopes: any[];
-    proofDeadline: number;
-    collateralToken: string;
-    challengeDeadline: number;
-    slopeStartingTime: number;
-    verificationContext: string;
-    verificationContract: string;
-    fillerCollateralAmount: number;
-    challengerCollateralAmount: number;
-  };
-  quoteContext: {
-    // TODO: should we add a solver identifier here?
-    toAsset: string;
-    toPrice: string;
-    discount: string;
-    fromAsset: string;
-    fromPrice: string;
-    intermediary: string;
-  };
-  settlementContractAddress: string;
-  swapperAddress: string;
-  nonce: number;
+interface Input {
+  token: string;
+  amount: number;
+}
+
+interface OutputDescription {
+  remoteOracle: string;
+  token: string;
+  amount: number;
+  recipient: string;
+  chainId: number;
+  remoteCall: string;
+}
+
+export interface LimitOrderData {
+  proofDeadline: number;
+  challengeDeadline: number;
+  collateralToken: string;
+  fillerCollateralAmount: number;
+  challengerCollateralAmount: number;
+  localOracle: string;
+  inputs: Input[];
+  outputs: OutputDescription[];
+}
+
+export interface DutchOrderData {
+  verificationContext: string;
+  verificationContract: string;
+  proofDeadline: number;
+  challengeDeadline: number;
+  collateralToken: string;
+  fillerCollateralAmount: number;
+  challengerCollateralAmount: number;
+  localOracle: string;
+  slopeStartingTime: number;
+  inputSlopes: string[];
+  outputSlopes: string[];
+  inputs: Input[];
+  outputs: OutputDescription[];
+}
+
+// order data is encoded as bytes, we will prob not use but it's here in case
+export interface CrossChainOrderEncoded {
+  settlementContract: string;
+  swapper: string;
+  nonce: string;
   originChainId: number;
-  initiatedDeadline: number;
+  initiateDeadline: number;
   fillDeadline: number;
-  signature: string;
+  orderData: string;
+}
+
+export interface CrossChainOrder {
+  settlementContract: string;
+  swapper: string;
+  nonce: string;
+  originChainId: number;
+  initiateDeadline: number;
+  fillDeadline: number;
+  orderData: DutchOrderData | LimitOrderData;
+}
+
+export interface CatalystOrderData {
+  orderType: 'LimitOrder' | 'DutchAuction';
+  order: CrossChainOrder;
 }
