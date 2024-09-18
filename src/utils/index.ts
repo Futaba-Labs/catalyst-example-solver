1e100d3bb985ccc1ce215314282ab7e3c7c185ee
Vladimir Agaev <agaev.vladimir@gmail.com>
2024-09-18 02:33:09 +0200

    chore: add utils & types for permit2 signature

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,72 @@
+import { bech32, bech32m } from 'bech32';
+import bs58check from 'bs58check';
+import { AddressType } from 'bitcoin-address-validation';
+
+export function isFromBTCToEvm(asset: string) {
+  return (
+    asset.toLowerCase() ===
+    '0x000000000000000000000000bc00000000000000000000000000000000000101'
+  );
+}
+
 export function formatRemoteOracleAddress(remoteOracle: string): string {
   const noPrefix = remoteOracle.replace('0x', '').toLowerCase();
   return `0x${noPrefix.padStart(64, '0')}`;
+}
+
+export function getBitcoinAddressVersion(
+  addressType: AddressType,
+): 1 | 2 | 3 | 4 | 5 {
+  switch (addressType) {
+    case AddressType.p2pkh:
+      return 1;
+    case AddressType.p2sh:
+      return 2;
+    case AddressType.p2wpkh:
+      return 3;
+    case AddressType.p2wsh:
+      return 4;
+    case AddressType.p2tr:
+      return 5;
+    default:
+      throw new Error(`Unsupported address type: ${addressType}`);
+  }
+}
+
+export function getSwapRecipientFromAddress(
+  address: string,
+  type: AddressType,
+): string {
+  switch (type) {
+    case AddressType.p2pkh:
+    case AddressType.p2sh: {
+      const decodedData = bs58check.decode(address);
+      const unPaddedRecipient = decodedData
+        .slice(1)
+        .reduce((t, x) => t + x.toString(16).padStart(2, '0'), '');
+      const paddedRecipient = unPaddedRecipient.padEnd(64, '0');
+      return '0x' + paddedRecipient;
+    }
+
+    case AddressType.p2wpkh:
+    case AddressType.p2wsh: {
+      const decodedData = bech32.decode(address);
+      const unPaddedRecipient = bech32
+        .fromWords(decodedData.words.slice(1))
+        .reduce((t, x) => t + x.toString(16).padStart(2, '0'), '');
+      const paddedRecipient = unPaddedRecipient.padEnd(64, '0');
+      return '0x' + paddedRecipient;
+    }
+
+    case AddressType.p2tr: {
+      const decodedData = bech32m.decode(address);
+      const recipient = bech32
+        .fromWords(decodedData.words.slice(1))
+        .reduce((t, x) => t + x.toString(16).padStart(2, '0'), '');
+      return '0x' + recipient;
+    }
+
+    default:
+      throw new Error('Unsupported address type');
+  }
 }
